cf439125b3db27e8f64145449f934133854dc5b2
jahid <info.jahidulislamjahid@gmail.com>
2023-07-20 14:14:09 +0600

    initial test

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
