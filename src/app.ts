9b4a32bedc1d1c3f04145fa1cd95a2d87d81343c
jahid <info.jahidulislamjahid@gmail.com>
2023-07-20 14:33:50 +0600

    building middleware skeleton

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import cors from 'cors';
+import express, { Application } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import router from './app/routes';
+
+const app: Application = express();
+
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Application routes
+app.use('/api/v1', router);
+
+app.get('/', async (req, res) => {
+  res.send('Welcome to Book Catalog Application Server 📚');
+});
+
+//client error handler
+app.use(globalErrorHandler);
+//handle Not Found ROute
+app.use((req, res) => {
+  res.status(StatusCodes.NOT_FOUND).json({
+    success: false,
+    message: 'Route Not Found',
+    errorMessage: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+});
+
+export default app;
---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,7 @@
+const globalErrorHnadler = () => {
+
+}
+
+
+
+export default globalErrorHnadler
