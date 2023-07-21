df16ea6a199d0fe45d47f259fbf09e26913d277a
jahid <info.jahidulislamjahid@gmail.com>
2023-07-22 01:52:30 +0600

    server not working properly

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod';
+
+const createUserZodSchema = z.object({
+  body: z.object({
+    firstName: z.string({
+      required_error: 'firstName is required ',
+    }),
+    lastName: z.string({
+      required_error: 'lastName is required ',
+    }),
+    email: z.string({
+      required_error: 'Email is required ',
+    }),
+    password: z.string({
+      required_error: 'Password is required ',
+    }),
+  }),
+});
+const updateUserZodSchema = z.object({
+  body: z.object({
+    name: z.object({
+      firstName: z.string().optional(),
+      lastName: z.string().optional(),
+    }),
+  }),
+});
+
+export const UserValidaion = {
+  updateUserZodSchema,
+  createUserZodSchema,
+};
---
+++ src/handlingError/apiError.ts
@@ -0,0 +1,13 @@
+export class ApiError extends Error {
+  status: number;
+  constructor(status: number, message: string, stack = '') {
+    super(message);
+    this.name = this.constructor.name;
+    this.status = status;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
